92d8eef434ff1c0e0c1ebb8ae063b6e0fc8e7fa8
mindistcalm <suxinin4@yandex.ru>
2024-06-01 01:15:15 +0700

    added build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { motion } from 'framer-motion'
 import { MCard } from './components'
